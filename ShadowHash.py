69cf4f46f1e34747910d9f0dcd7a1fb297c2f9da
0xffxd <xephoxx@protonmail.com>
2018-06-09 19:47:30 +0200

    commit
    First version

---
+++ ShadowHash.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+# Title           : ShadowHash.py
+# Description     : Generating the hash of password stored in /etc/shadow
+# Version         : 1.0
+# Python_version  : 2.7
+
+import crypt
+
+print crypt.crypt('doit0002', '$6$kURThR6P')
