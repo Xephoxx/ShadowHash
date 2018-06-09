9fcc4838403f1c15e5ab1452abc0831b39170248
0xffxd <xephoxx@protonmail.com>
2018-06-09 22:39:25 +0200

    Update and rename ShadowHash.py to shadow_hash.py

--- shadow_hash.py
+++ shadow_hash.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python2.7
 # -*- coding: utf-8 -*-
-# Title           : ShadowHash.py
+# Title           : Shadow_Hash.py
 # Description     : Generating the hash of password stored in /etc/shadow
 # Version         : 1.0
 # Python_version  : 2.7
